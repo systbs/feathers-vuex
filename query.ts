c032b4b203c634f9ec71a621a6c519e20989f71c
Yasser Sajjadi <Yasser-Sajjadi@outlook.com>
2021-10-04 14:58:10 +0330

    Update query.ts

--- query.ts
+++ query.ts
@@ -1,104 +1,406 @@
-import { Params } from '@feathersjs/feathers';
-import { merge, omit } from 'lodash';
-import { Ref, isRef, computed, ref, watch } from 'vue';
+import { ActionTree, GetterTree, Module, MutationTree, Store } from 'vuex';
+import { filterQuery, sorter, select } from '@feathersjs/adapter-commons';
+import { unref, reactive } from 'vue';
+import { omit, get, pick, merge } from 'lodash';
+import { Service, Application } from '@feathersjs/feathers';
+import sift from 'sift';
+import { StateInterface } from './types';
 import { Model } from './model';
+import { getServicePath, ns, assignIfNotPresent } from './utils';
 
-interface QueryOptions {
-	model: typeof Model;
-	method?: 'find' | 'get';
-	params?: Params | Ref<Params>,
-	hooks?: any;
-}
+const FILTERS = ['$sort', '$limit', '$skip', '$select']
+const additionalOperators = ['$elemMatch']
+const blacklist = [
+	'options',
+];
 
-interface QueryResult<T = any> {
-	items?: Ref<T[]> | Ref<T>;
-	paginationData?: Ref<Record<any, any>>;
+export interface ServicePluginOptions {
+	model: any;
+	service: Service<any>;
+
+	idField?: string;
+	modelName?: string;
+
 	servicePath?: string;
+	namespace?: string;
+
+	whitelist?: string[];
+	paramsForServer?: string[];
+
+	state?: any;
+	getters?: any;
+	mutations?: any;
+	actions?: any;
+
+	debounceEventsMaxWait?: number;
+	enableEvents?: boolean;
+}
+
+export interface FeathersVuexOptions {
+	namespace?: string;
+	serverAlias?: string;
+	idField?: string;
+	paramsForServer?: string[];
+	whitelist?: string[];
+	debug?: boolean;
+	debounceEventsMaxWait?: number;
+}
+
+export interface FeathersVuexOptionsInstance extends FeathersVuexOptions {
+	namespace: string;
+	serverAlias: string;
+	idField: string;
+	paramsForServer: string[];
+	whitelist: string[];
+	debug: boolean;
+	debounceEventsMaxWait: number;
+}
+
+export default class FeathersVuex {
 	[key: string]: any;
-}
 
-function isPaginated(item: any) {
-	return item && ('total' in item) && ('limit' in item) &&
-		('skip' in item) && ('data' in item);
-}
+	models: {
+		[k: string]: any;
+	};
 
-const unwrap = <T>(params: T | Ref<T>): T =>
-	isRef(params) ? params.value : params;
+	options: FeathersVuexOptionsInstance;
 
-export async function useQuery<M extends Model>(options: QueryOptions) {
-	const { model, method, params, hooks } = merge({
-		method: 'find',
-		params: {},
-		hooks: (record: any, context: any) => record
-	}, options);
+	#defaults: FeathersVuexOptionsInstance = {
+		namespace: '',
+		serverAlias: '',
+		idField: '_id',
+		paramsForServer: [],
+		whitelist: [],
+		debug: false,
+		debounceEventsMaxWait: 1000
+	};
 
-	if (!model) {
-		throw new Error('No model provided for useQuery()');
+	app: Application;
+
+	constructor(app: Application<any>, config: FeathersVuexOptions) {
+		this.options = Object.assign(this.#defaults, config);
+		this.models = {};
+		this.app = app;
 	}
 
-	function useHook(item: any, context: any) {
-		if (hooks) {
-			if (typeof hooks === 'function') {
-				hooks(item, context);
-			}
-			if (Array.isArray(hooks)) {
-				for (const hook of hooks) {
-					if (typeof hook === 'function') {
-						hook(item, context);
+	private insert(model: any) {
+		const { serverAlias, debug } = this.options;
+
+		this.models[serverAlias] = this.models[serverAlias] || {
+			byServicePath: {}
+		}
+
+		const name = String(model.modelName || model.name);
+
+		if (this.models[serverAlias][name] && debug) {
+			console.error(`Overwriting model: models[${serverAlias}][${name}].`);
+		}
+
+		this.models[serverAlias][name] = model;
+		this.models[serverAlias].byServicePath[model.servicePath] = model;
+	}
+
+	private clear() {
+		Object.keys(this.models).forEach(key => {
+			const serverAliasObj = this.models[key];
+			Object.keys(serverAliasObj).forEach(key => {
+				delete this.models[key];
+			});
+			delete this.models[key];
+		})
+	}
+
+	private remove(name: string) {
+		const { serverAlias, debug } = this.options;
+		if (!this.models[serverAlias][name] && debug) {
+			console.error(`Overwriting model: models[${serverAlias}][${name}].`);
+		}
+		delete this.models[serverAlias][name];
+	}
+
+	createModule<S extends StateInterface = any, R = any>(
+		service: Service<any>, options: ServicePluginOptions, store: Store<any>
+	) {
+		const getters: GetterTree<S, R> = {
+			find(state) {
+				const {
+					paramsForServer,
+					whitelist,
+					keyedById,
+				} = state
+
+				return (payload: any) => {
+					const params = unref(payload) || {};
+					const q = omit(params.query || {}, paramsForServer);
+
+					const { query, filters } = filterQuery(q, {
+						operators: additionalOperators.concat(whitelist)
+					});
+
+					let values = Object.values<any>(keyedById);
+
+
+					values = values.filter(sift(query))
+
+					const total = values.length;
+
+					if (filters.$sort !== undefined) {
+						values.sort(sorter(filters.$sort));
+					}
+
+					if (filters.$skip !== undefined && filters.$limit !== undefined) {
+						values = values.slice(filters.$skip, Number(filters.$limit) + Number(filters.$skip))
+					} else if (filters.$skip !== undefined || filters.$limit !== undefined) {
+						values = values.slice(filters.$skip, filters.$limit)
+					}
+
+					if (filters.$select) {
+						values = select(params)(values)
+					}
+
+					return {
+						total,
+						limit: filters.$limit || 0,
+						skip: filters.$skip || 0,
+						data: values
+					}
+				}
+			},
+			get(state) {
+				const { keyedById, idField } = state;
+				return (payload: any) => {
+					const { id, params } = payload;
+					return keyedById[id] && select(params, idField)(keyedById[id]);
+				}
+			},
+			count(state, getters) {
+				return (payload: any) => {
+					const params = unref(payload) || {};
+
+					const cleanQuery = omit(params.query, FILTERS)
+					params.query = cleanQuery;
+
+					return getters.find(params).total;
+				}
+			},
+		}
+
+		const mutations: MutationTree<S> = {
+			update: (state, payload) => {
+				const { keyedById, idField } = state;
+				const items = Array.isArray(payload) ? payload : [payload];
+				for (let item of items) {
+					const id = item[idField];
+					if (id !== null && id !== undefined) {
+						if (id in keyedById) {
+							for (const key in item) {
+								if (item[key] !== keyedById[id][key]) {
+									Reflect.set(keyedById[id], key, item[key]);
+								}
+							}
+						} else {
+							const { serverAlias, modelName } = state
+							const model = get(this.models, [serverAlias, modelName]);
+
+							item = reactive(merge({}, omit(item, blacklist)));
+
+							keyedById[id] = new model(item);
+						}
+					}
+				}
+			},
+			remove(state, payload) {
+				const { keyedById, idField } = state;
+				const items = Array.isArray(payload) ? payload : [payload];
+				for (const item of items) {
+					const id = item[idField];
+					if (id !== null && id !== undefined) {
+						if (id in keyedById) {
+							unref(item[id]);
+							delete item[id];
+						}
+					}
+				}
+			},
+			upgrade: (state, payload) => {
+				const { keyedById, idField } = state;
+				const items = Array.isArray(payload) ? payload : [payload];
+				for (let item of items) {
+					const id = item[idField];
+					if (id !== null && id !== undefined) {
+						const { serverAlias, modelName } = state
+						const model = get(this.models, [serverAlias, modelName]);
+
+						item = reactive(merge({}, omit(item, blacklist)));
+
+						keyedById[id] = new model(item);
 					}
 				}
 			}
-		}
-	}
+		};
 
-	const response = ref<QueryResult<M>>({});
+		const actions: ActionTree<S, R> = {
+			async create({ commit, getters, state }, payload) {
+				const { data, params } = payload;
+				const response = await service.create(data, params);
+				commit('update', response);
+				const { idField } = state;
+				return getters.get(response[idField]);
+			},
 
-	const computes = {
-		items: computed(() => {
-			if (Array.isArray(response.value)) {
-				for (const item of response.value) {
-					useHook(item, { model, method, params });
+			async update({ commit, getters }, payload) {
+				const { id, data, params } = payload;
+				const response = await service.update(id, data, params);
+				commit('update', response);
+				return getters.get(payload);
+			},
+
+			async find({ commit, getters }, payload) {
+				const response = await service.find(payload);
+				if (Array.isArray(response)) {
+					for (const item of response) {
+						commit('update', item);
+					}
+				} else {
+					for (const item of response.data) {
+						commit('update', item);
+					}
 				}
-				return response.value;
-			}
-			else if (isPaginated(response.value)) {
-				for (const item of response.value.data) {
-					useHook(item, { model, method, params });
+				return getters.find(payload);
+			},
+
+			async get({ commit, getters }, payload) {
+				const record = getters.get(payload);
+				if (record !== undefined && record !== null) {
+					return record;
 				}
-				return response.value.data;
-			}
-			else {
-				useHook(response.value, { model, method, params });
-				return response.value ? [response.value] : [];
-			}
-		}),
-		paginationData: computed(() => {
-			if ('data' in response.value) {
-				return omit(response.value, 'data');
-			}
-			return { total: 0, skip: 0, limit: 0 };
-		}),
-		servicePath: computed<string>(() => model.servicePath)
+				const { id, params } = payload;
+				const response = await service.get(id, params);
+				if (response) {
+					commit('update', response);
+				}
+				return getters.get(payload);
+			},
+
+			async remove({ commit }, payload) {
+				const { id, params } = payload;
+				const response = await service.remove(id, params);
+				if (response) {
+					commit('remove', response);
+				}
+				return response;
+			},
+
+			async patch({ commit, getters }, payload) {
+				const { id, data, params } = payload;
+				const response = await service.patch(id, data, params);
+				commit('update', response);
+				return getters.get(payload);
+			},
+		}
+
+		const fromOptions = pick(options, [
+			'state',
+			'getters',
+			'mutations',
+			'actions',
+			'namespace',
+			'modelName',
+			'idField',
+			'servicePath',
+			'serverAlias'
+		]);
+
+		const state: any = merge({
+			keyedById: {},
+			namespace: '',
+			pagination: {
+				defaultLimit: 0,
+				defaultSkip: 0,
+			},
+			whitelist: [],
+			paramsForServer: [],
+			modelName: '',
+			idField: '_id'
+		}, fromOptions);
+
+		const defaults: Module<S, R> = {
+			namespaced: true,
+			actions,
+			getters,
+			mutations,
+			state
+		};
+
+		const merged = merge({}, defaults, fromOptions);
+
+		return merge({}, merged, { store, module: merged });
 	}
 
-	async function fetch(getterParams: any) {
-		if (method && method.toLowerCase() === 'get') {
-			const { id, params: paramsGet } = getterParams;
-			response.value = await model.get(id, paramsGet);
-		} else {
-			response.value = await model.find(getterParams);
+	enableServiceEvents(
+		service: Service<any>,
+		store: Store<any>,
+		options: FeathersVuexOptionsInstance & ServicePluginOptions
+	) {
+		const { namespace } = options;
+		service.on('created', (item: any) => {
+			store.commit(`${namespace}/update`, item);
+		})
+		service.on('updated', item => {
+			store.commit(`${namespace}/update`, item);
+		})
+		service.on('patched', item => {
+			store.commit(`${namespace}/update`, item);
+		})
+		service.on('removed', item => {
+			store.commit(`${namespace}/remove`, item);
+		})
+	}
+
+	createServicePlugin(config: ServicePluginOptions) {
+		const options = Object.assign({}, this.#defaults, this.options, config);
+		const {
+			model,
+			service,
+			namespace
+		} = options;
+		let { servicePath } = options;
+		if (!servicePath) {
+			servicePath = getServicePath(service, model);
+		}
+
+		options.servicePath = servicePath;
+		options.modelName = model.modelName;
+
+		return (store: Store<any>) => {
+			options.namespace = ns(namespace, servicePath);
+
+			const module = this.createModule(service, options, store);
+			store.registerModule(options.namespace, module, { preserveState: false });
+
+			const BaseModel = get(this.models, [this.options.serverAlias, 'Model']);
+			if (BaseModel && !BaseModel.store) {
+				Object.assign(BaseModel, { store });
+			}
+
+			assignIfNotPresent(model, {
+				namespace: options.namespace,
+				servicePath,
+				idField: options.idField
+			});
+
+			Object.assign(model, { store });
+
+			if (!model.modelName || model.modelName === 'Model') {
+				throw new Error('The modelName property is required for Models');
+			}
+
+			this.insert(model);
+
+			if (options.enableEvents) {
+				this.enableServiceEvents(service, store, options);
+			}
 		}
 	}
 
-	watch(() => unwrap(params), async (value) => {
-		if (value !== undefined) {
-			await fetch(unwrap(params));
-		}
-	});
-
-	await fetch(unwrap(params))
-
-	return computes;
 }
-
-export default useQuery;
